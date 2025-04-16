4db1c9bfee21021402fc57ac8a0594e3145f1d21
acheltonzuzi <acheltonzuzi@gmail.com>
2025-04-16 09:55:07 +0100

    Implement chatbot functionality with state management and tool integration

---
+++ memory.py
@@ -0,0 +1,53 @@
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import BaseMessage
+from typing_extensions import TypedDict
+from typing import Annotated
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_core.messages import ToolMessage
+
+load_dotenv()
+memory = MemorySaver()
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+
+tool = TavilySearchResults(max_results=2)
+tools = [tool]
+llm=ChatGroq(model="deepseek-r1-distill-llama-70b",temperature=1)
+
+llm_with_tools = llm.bind_tools(tools)
+
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+
+graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+graph = graph_builder.compile(checkpointer=memory)
+
+config = {"configurable": {"thread_id": "1"}}
+while True:
+    input_message = input("User: ")
+    res=graph.invoke({"messages": [{"role": "user", "content": input_message}]},config)
+    print(res["messages"][-1].content)
+    
